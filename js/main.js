d527fd62241428b414cae774c9f314298980ad16
Мария Гаврилова <topik2344@gmail.com>
2022-09-30 22:14:34 +0300

    Добавляе функцию, исправляет по замечаниям

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-function getRandomCoordinate (min, max, decimal) {
-  if (min < 0 || max < 0 || decimal < 0 || max <= min) {
+const getRandomFloat = (min, max, decimal) => {
+  if (min < 0 || decimal < 0 || max <= min) {
     return NaN;
   }
-  const randomCoordinate = Math.random() * (max - min) + min;
-  return randomCoordinate.toFixed(decimal);
-}
+  const random = Math.random() * (max + (0.1 ** decimal) - min) + min;
+  return Number(random.toFixed(decimal));
+};
 
-getRandomCoordinate(2, 4, 3);
+getRandomFloat(2, 4, 3);
+
+const getRandomInt = (min, max) => {
+  if (min < 0 || max <= min) {
+    return NaN;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+getRandomInt(2, 4);
