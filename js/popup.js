b30d63e420b5c0e2bed7958638982264478ad3f8
Мария Гаврилова <topik2344@gmail.com>
2022-10-12 20:27:33 +0300

    Добавляет элементы объекта на страницу

--- js/popup.js
+++ js/popup.js
@@ -31,6 +31,22 @@ similarCards.forEach((popup) => {
   };
 
   cardElement.querySelector('.popup__type').textContent = createAccomodationType(popup.offer.type);
+  cardElement.querySelector('.popup__text--capacity').textContent = `${popup.offer.rooms} комнаты для ${popup.offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${popup.offer.checkin}, выезд до ${popup.offer.checkout}`;
+  cardElement.querySelector('.popup__features').textContent = popup.offer.features;
+  cardElement.querySelector('.popup__description').textContent = popup.offer.description;
+
+  const popupPhotosElement = cardElement.querySelector('.popup__photos');
+  popupPhotosElement.innerHTML = '';
+
+  popup.offer.photos.forEach((photo) => {
+    const imageElement = document.createElement('img');
+    imageElement.src = photo;
+    imageElement.width = '45';
+    imageElement.height = '40';
+    imageElement.alt = 'Фотография жилья';
+    popupPhotosElement.append(imageElement);
+  });
   cardElement.querySelector('.popup__avatar').src = popup.author.avatar;
   mapCanvas.appendChild(cardElement);
 });
